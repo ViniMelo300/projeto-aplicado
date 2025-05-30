eaace504455a1351b67ef14794945cc7bc5b1315
ViniMelo300 <gamesleonar@gmail.com>
2025-05-30 16:01:47 -0300

    criação dos cards, btn carrinho e teste de funcionalidades

--- projetoApli/main.js
+++ projetoApli/main.js
@@ -1,3 +1,14 @@
 function mostrarmensagem(){
     alert("Você clicou no botão");
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    const btnCarrinho = document.getElementById('btn-acao');
+
+    if(btnCarrinho) {
+        btnCarrinho.addEventListener('click', function() {
+            alert('O botão do carrinho foi clicado!');
+            console.log("botão clicado", this.id);
+        })
+    }
+})
